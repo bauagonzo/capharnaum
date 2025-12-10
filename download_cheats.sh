be97b38add21910e028e9bf617249aa2f27e1399
Baua Gonzo <baua.gonzo@gmail.com>
2025-12-10 15:30:34 +0100

    use main branch for tldr repo

--- download_cheats.sh
+++ download_cheats.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 TMPDIR=${TMPDIR:-/tmp}
 cd $TMPDIR
-curl -LOk https://github.com/tldr-pages/tldr/archive/master.zip
-unzip master.zip
+curl -LOk https://github.com/tldr-pages/tldr/archive/main.zip
+unzip main.zip
 mkdir -p cheats
-cp -rf tldr-master/pages/linux/* cheats
-cp -rf tldr-master/pages/common/* cheats
+cp -rf tldr-main/pages/linux/* cheats
+cp -rf tldr-main/pages/common/* cheats
 cd cheats
 rename .md '' *.md
 cd ..
 cp -rf cheats/* ~/.cheat-tldr
-rm -rf master.zip tldr-master cheats
-
+rm -rf main.zip tldr-main cheats
